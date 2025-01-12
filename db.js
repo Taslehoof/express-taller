3e393b8b6611a92ce73e5c5baa40d4882b807a27
Sergio Conti <seracon@gmail.com>
2025-01-12 10:08:20 -0300

    Changes and updates
    Changes in the Database and connections to a MySQL not to a SQLite

--- db.js
+++ db.js
@@ -1,25 +1,38 @@
-import sqlite3 from 'sqlite3'
-import { open } from 'sqlite'
-
-open({
-    filename:'/home/Taslehoof/SQLite',
-    driver: sqlite3.Database
-}).then((db) => {
-    const result =  connection.query('SELECT 1 + 1 AS Result')
-    console.log(result)
-
-})
-
-//function connectDB(){
+//import sqlite3 from 'sqlite3'
+//import { open } from 'sqlite'
 //
-//    const connection =  sqlite.createConnection({
-//        host:'localhost',
-//        database:'SQLite3/Personas'
-//    })
-//
+//open({
+//    filename:'/home/Taslehoof/SQLite',
+//    driver: sqlite3.Database
+//}).then((db) => {
 //    const result =  connection.query('SELECT 1 + 1 AS Result')
 //    console.log(result)
 //
-//}
-//
-//module.exports = connectDB
+//})
+
+
+//import mysql from 'mysql2/promise';
+const mysql = require('mysql2/promise');
+
+async function connectDB(){
+
+    try{
+        const connection = await mysql.createConnection({
+            host:'localhost',
+            user: 'sconti',
+            password: 'contiSA/*/9685',
+            database:'sconti'
+        })
+        console.log('Conexion a MySQL establecida.')
+        return connection;
+    } catch (error) {
+        console.log('Error al conectar a MySQL')
+    }
+
+    const result = connection.query('SELECT 1 + 1 AS Result')
+    console.log(result)
+
+}
+
+//export default connectDB;
+module.exports = connectDB
